19e9a60ab0053a3d213a3d40453b2229eac03e9a
«rtm7777» <kolan678@gmail.com>
2013-12-08 21:53:16 +0200

    commit)

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ function draw() {
 	var canvas = document.getElementById("game_canvas");
 	var ctx = canvas.getContext("2d");
 
+	canvas.width = canvas.clientWidth;
+	canvas.height = canvas.clientHeight;
+
 	ctx.fillStyle = "rgba(40, 40, 40, 0.6)";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -10,6 +13,18 @@ function draw() {
 
 	ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
 	ctx.fillRect(30, 30, 55, 50);
+
+	ctx.fillStyle = "rgb(200,0,0)";
+	ctx.fillRect(canvas.width - 10, 10, -55, 50);
+
+	ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+	ctx.fillRect(canvas.width - 30, 30, -55, 50);
+
+	ctx.fillStyle = "#03A";
+    ctx.font = "italic 30pt Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "center";
+    ctx.fillText("Once here will be a game :)", canvas.width/2, canvas.height/2);
 }
 
 $(function() {

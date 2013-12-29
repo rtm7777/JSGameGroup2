a6a6b2dc4bb6f6b27b3cf44cf079dc454b4bb266
«rtm7777» <kolan678@gmail.com>
2013-12-29 22:11:38 +0200

    added map

--- js/main.js
+++ js/main.js
@@ -1,32 +1,157 @@
-function draw() {
-	var canvas = document.getElementById("game_canvas");
-	var ctx = canvas.getContext("2d");
-
-	canvas.width = canvas.clientWidth;
-	canvas.height = canvas.clientHeight;
-
-	ctx.fillStyle = "rgba(40, 40, 40, 0.6)";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-	ctx.fillStyle = "rgb(200,0,0)";
-	ctx.fillRect(10, 10, 55, 50);
-
-	ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-	ctx.fillRect(30, 30, 55, 50);
-
-	ctx.fillStyle = "rgb(200,0,0)";
-	ctx.fillRect(canvas.width - 10, 10, -55, 50);
-
-	ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-	ctx.fillRect(canvas.width - 30, 30, -55, 50);
-
-	ctx.fillStyle = "#03A";
-    ctx.font = "italic 30pt Arial";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "center";
-    ctx.fillText("Once here will be a game :)", canvas.width/2, canvas.height/2);
-}
-
 $(function() {
-	draw();
+
+	// 0 - grass
+	// 1 - pebble
+	// 2 - water
+	// 3 - lawa
+
+	var mapArray = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+					[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 2, 2],
+					[1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+					[0, 1, 1, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+					[0, 0, 1, 1, 1, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+					[0, 0, 0, 1, 1, 1, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+					[0, 0, 0, 0, 1, 1, 1, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+					[0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 0, 0, 0, 1],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 3, 0, 0, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					[2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+
+	var step = 10;
+
+	var stage = new Kinetic.Stage({
+		container: 'game',
+		width: 700,
+		height: 500
+	});
+
+	var layer = new Kinetic.Layer();
+	stage.add(layer);
+	var mapLayer = new Kinetic.Layer();
+	stage.add(mapLayer);
+
+	var sources = {
+		0: 'img/grass.png',
+		1: 'img/pebble.png',
+		2: 'img/water.png',
+		3: 'img/lava.png'
+	};
+
+	function loadImages(sources, callback) {
+		var images = {};
+		var numImages = 0;
+		var loadedImages = 0;
+
+		for(var src in sources) {
+			numImages++;
+		}
+
+		for (var src in sources) {
+			images[src] = new Image();
+			images[src].onload = function() {
+				if(++loadedImages >= numImages) {
+	              callback(images);
+	            }
+			};
+			images[src].src = sources[src];
+		}
+	};
+
+	var player1 = new Kinetic.Rect({
+		x: 40,
+		y: 40,
+		width: 20,
+		height: 20,
+		fill: 'green',
+		level: 40,
+	});
+	layer.add(player1);
+	layer.draw();
+
+	function drawMap(mapArray, images) {
+		for (col in mapArray) {
+			for (row in mapArray[col]) {
+				var res = new Kinetic.Image({
+					x: row*20,
+					y: col*20,
+					image: images[mapArray[col][row]]
+				});
+				mapLayer.add(res);
+			}
+		}
+		mapLayer.moveToBottom();
+		mapLayer.draw();
+	};		
+
+
+	$(document).keydown(function(event) {
+		if (event.keyCode == 37) {
+			if (player1.getX() > 0) {
+				movePlayer(player1, "left");
+			};
+
+		} else if (event.keyCode == 38) {
+
+			if (player1.getY() > 0) {
+				movePlayer(player1, "up");
+			};
+
+		} else if (event.keyCode == 39) {
+
+			if (player1.getX() < stage.getWidth() - 20) {
+				movePlayer(player1, "right");
+			};
+
+		} else if (event.keyCode == 40) {
+
+			if (player1.getY() < stage.getHeight() - 20) {
+				movePlayer(player1, "down");
+			};
+
+		};
+
+		layer.draw();
+		event.preventDefault();
+
+	});
+
+	function movePlayer(player, direction) {
+		switch (direction) {
+			case "left":
+				player.setX(player.getX() - step);
+				break;
+			case "up":
+				player.setY(player.getY() - step);
+				break;
+			case "right":
+				player.setX(player.getX() + step);
+				break;
+			case "down":
+				player.setY(player.getY() + step);
+				break;
+		};
+	};
+
+	function addRes(resNum, posX, posY) {
+
+	}
+
+	loadImages(sources, function(images) {
+		drawMap(mapArray, images);
+	});
+
 });

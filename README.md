e14c42dc56be5bec6a298e2859573acbf79e6b49
rtm7777 <kolan678@gmail.com>
2014-02-19 17:24:03 +0200

    added login page

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ JSGameGroup2
 ============
 
 Оnline game on the canvas, websockets and node.js from group 2
+
+Please add environment variable NODE_ENV=development for correct work
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@ var HttpError = require('../error').HttpError;
 
 module.exports = function(app) {
 	app.get('/', function(req, res, next) {
-		res.render('index', {
+		res.render('game', {
 			title: "Best Game",
 			player: "Super Mario"
+		});
+	});
+
+	app.get('/login', function(req, res, next) {
+		res.render('login', {
+			title: "Login",
+			player: "Ezhik"
 		});
 	});
 }
